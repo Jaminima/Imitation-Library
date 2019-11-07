955f7247bfebfd28973d360d70db597b263ee86e
Oscar Davies <oscar.william.davies@outlook.com>
2019-11-07 13:37:21 +0000

    Start of Linq Imitation

---
+++ ImitationLibrary/Linq.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ImitationLibrary
+{
+    public static class Linq
+    {
+        public static int Count<T>(T[] Arr, Func<T, bool> Check)
+        {
+            int C = 0;
+            foreach (T item in Arr)
+            {
+                if (Check(item)) { C++; }
+            }
+            return C;
+        }
+
+        public static T[] Where<T>(T[] Arr, Func<T, bool> Check)
+        {
+            T[] Set = new T[Count<T>(Arr, Check)];
+            int Pos = 0;
+            foreach (T item in Arr)
+            {
+                if (Check(item)) { Set[Pos] = item; Pos++; }
+            }
+            return Set;
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using ImitationLibrary;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] A = new int[] { 1, 5, 9, 2 };
+            int p = Linq.Count<int>(A, x => x < 6);
+            int[] B = Linq.Where<int>(A, x => x < 6);
+        }
+    }
+}
