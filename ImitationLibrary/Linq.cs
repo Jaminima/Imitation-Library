827216d69c0ef22853ec5c6dd6457d2f98bb3393
Oscar Davies <oscar.william.davies@outlook.com>
2019-11-07 21:56:16 +0000

    Contains, Reverse, Sort

--- ImitationLibrary/Linq.cs
+++ ImitationLibrary/Linq.cs
@@ -25,14 +25,54 @@ namespace ImitationLibrary
             return Set;
         }
 
-        public static T Max<T>(T[] Arr, Func<T, int> Check)
+        public static T[] Max<T>(T[] Arr, Func<T, int> Value)
         {
             T Top = Arr[0];
             foreach (T item in Arr)
             {
-                if (Check(Top) < Check(item)) { Top = item; }
+                if (Value(Top) < Value(item)) { Top = item; }
             }
-            return Top;
+            return Where(Arr, x => Value(x) == Value(Top));
+        }
+
+        public static bool Contains<T>(T[] Arr, Func<T, bool> Check)
+        {
+            foreach (T item in Arr)
+            {
+                if (Check(item)) { return true; }
+            }
+            return false;
+        }
+
+        public static void Reverse<T>(T[] Arr)
+        {
+            T Temp; int Length = Arr.Length;
+            for (int i = 0; i < (int)Math.Floor((decimal)Length / 2); i++)
+            {
+                Temp = Arr[i];
+                Arr[i] = Arr[Length - i - 1];
+                Arr[Length - i - 1] = Temp;
+            }
+        }
+
+        public static void Sort<T>(T[] Arr, Func<T, int> Value)
+        {
+            T Temp; bool SwapOccurred = true; int K = 1;
+            while (SwapOccurred)
+            {
+                SwapOccurred = false;
+                for (int i = 0; i < Arr.Length - K; i++)
+                {
+                    if (Value(Arr[i]) > Value(Arr[i + 1]))
+                    {
+                        Temp = Arr[i];
+                        Arr[i] = Arr[i + 1];
+                        Arr[i + 1] = Temp;
+                        SwapOccurred = true;
+                    }
+                }
+                K++;
+            }
         }
     }
 }
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -7,9 +7,11 @@ namespace TestApp
         private static void Main(string[] args)
         {
             int[] A = new int[] { 1, 5, 9, 2 };
-            int Count = Linq.Count<int>(A, x => x < 6);
-            int[] Where = Linq.Where<int>(A, x => x < 6);
-            int Max = Linq.Max<int>(A, x => x);
+            int Count = Linq.Count(A, x => x < 6);
+            int[] Where = Linq.Where(A, x => x < 6);
+            int[] Max = Linq.Max(A, x => x);
+            Linq.Sort(A, x => x);
+            Linq.Reverse(A);
         }
     }
 }
