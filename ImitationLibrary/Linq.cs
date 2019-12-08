6e90e06ede016f40a30304450b8c4525b8c856b8
Oscar Davies <oscar.william.davies@outlook.com>
2019-12-08 15:00:52 +0000

    Add comments

--- ImitationLibrary/Linq.cs
+++ ImitationLibrary/Linq.cs
@@ -6,27 +6,27 @@ namespace ImitationLibrary
     {
         public static void ForEach<T>(T[] Arr, Func<T, T> Function)
         {
-            if (Arr.Length == 0) { return; }
+            if (Arr.Length == 0) { return; }//Ensure Array has items to operate on
             for (int i = 0; i < Arr.Length; i++)
-            {
+            { //For every item perform the function and overwrite the item in the array
                 Arr[i] = Function(Arr[i]);
             }
         }
 
         public static void ForEach<T>(T[] Arr, Action<T> Function)
         {
-            if (Arr.Length == 0) { return; }
+            if (Arr.Length == 0) { return; }//Ensure Array has items to operate on
             for (int i = 0; i < Arr.Length; i++)
-            {
+            { //For every item in the array, perform the function on it
                 Function(Arr[i]);
             }
         }
 
         public static int Count<T>(T[] Arr, Func<T, bool> Check)
         {
-            int C = 0;
+            int C = 0;//Store the count
             foreach (T item in Arr)
-            {
+            {//Increase the count when an item meets the Check
                 if (Check(item)) { C++; }
             }
             return C;
@@ -34,10 +34,10 @@ namespace ImitationLibrary
 
         public static T[] Where<T>(T[] Arr, Func<T, bool> Check)
         {
-            T[] Set = new T[Count<T>(Arr, Check)];
+            T[] Set = new T[Count<T>(Arr, Check)];//Create a set as long as the amount of items that meet the case
             int Pos = 0;
             foreach (T item in Arr)
-            {
+            {//Add each item into the new array if they meet the check
                 if (Check(item)) { Set[Pos] = item; Pos++; }
             }
             return Set;
@@ -45,9 +45,9 @@ namespace ImitationLibrary
 
         public static int Sum<T>(T[] Arr, Func<T, int> Value)
         {
-            int Sum = 0;
+            int Sum = 0;//Store the sum
             foreach (T item in Arr)
-            {
+            {//Add the value of each item into the Sum
                 Sum += Value(item);
             }
             return Sum;
@@ -55,57 +55,58 @@ namespace ImitationLibrary
 
         public static T[] Max<T>(T[] Arr, Func<T, int> Value)
         {
-            T Top = Arr[0];
+            T Top = Arr[0];//Stores the current greatest value
             foreach (T item in Arr)
-            {
+            {//Check if the item is larger than Top, if it is, replace Top
                 if (Value(Top) < Value(item)) { Top = item; }
             }
-            return Where(Arr, x => Value(x) == Value(Top));
+            return Where(Arr, x => Value(x) == Value(Top)); //Return the objects with the largest value
         }
 
         public static T[] Min<T>(T[] Arr, Func<T, int> Value)
         {
-            T Bot = Arr[0];
+            T Bot = Arr[0];//Stores the current smallest value
             foreach (T item in Arr)
-            {
+            {//Check if the item is smaller than Bot, if it is, replace Bot
                 if (Value(Bot) > Value(item)) { Bot = item; }
             }
-            return Where(Arr, x => Value(x) == Value(Bot));
+            return Where(Arr, x => Value(x) == Value(Bot));//Return the objects with the smallest value
         }
 
         public static bool Contains<T>(T[] Arr, Func<T, bool> Check)
         {
             foreach (T item in Arr)
-            {
+            {//Check if the item matches the check, and return true if one does
                 if (Check(item)) { return true; }
             }
-            return false;
+            return false;//If no items match, return false
         }
 
         public static void Reverse<T>(T[] Arr)
         {
-            T Temp; int Length = Arr.Length;
-            for (int i = 0; i < (int)Math.Floor((decimal)Length / 2); i++)
+            T Temp;//Store a value during the swap
+            int Length = Arr.Length;
+            for (int i = 0; i < (int)Math.Floor((decimal)Length / 2); i++)//We only want to itterate through the first half of the array
             {
-                Temp = Arr[i];
-                Arr[i] = Arr[Length - i - 1];
-                Arr[Length - i - 1] = Temp;
+                Temp = Arr[i];//Store temporarily
+                Arr[i] = Arr[Length - i - 1];//Take from the second half of the array and put it in the first half
+                Arr[Length - i - 1] = Temp;//Take the temp value into the second half of the array
             }
         }
 
         public static T[][] Split<T>(T[] Arr, int Start, int Split, int End)
         {
-            T[][] Out = new T[2][];
-            Out[0] = new T[Split - Start];
+            T[][] Out = new T[2][];//Store the 2 parts of the array
+            Out[0] = new T[Split - Start];//Store all values between the start and Split of the arr into the first part of the array
             for (int i = Start; i < Split; i++) { Out[0][i - Start] = Arr[i]; }
-            Out[1] = new T[End - Split + 1];
+            Out[1] = new T[End - Split + 1];//Store all values between the split and end of the arr into the second part of the array
             for (int i = Split; i <= End; i++) { Out[1][i - Split] = Arr[i]; }
             return Out;
         }
 
         public static void Sort<T>(T[] Arr, Func<T, int> Value)
         {
-            Sorting.MergeSort(Arr, Value, 0, Arr.Length - 1);
+            Sorting.MergeSort(Arr, Value, 0, Arr.Length - 1);//Perform merge sort on the whole array
         }
     }
 }
--- ImitationLibrary/Sorting.cs
+++ ImitationLibrary/Sorting.cs
@@ -6,16 +6,16 @@ namespace ImitationLibrary
     {
         public static void MergeSort<T>(T[] Arr, Func<T, int> Value, int Left, int Right)
         {
-            int Middle = (int)Math.Ceiling((double)(Left + Right) / 2);
-            if (Right - Left > 1)
+            int Middle = (int)Math.Ceiling((double)(Left + Right) / 2);//Find the middle index of the array
+            if (Right - Left > 1)//Check to see if we have 3 or more values to sort
             {
-                MergeSort(Arr, Value, Left, Middle - 1);
-                MergeSort(Arr, Value, Middle, Right);
-                Merge(Arr, Left, Linq.Split(Arr, Left, Middle, Right), Value);
+                MergeSort(Arr, Value, Left, Middle - 1);//Merge sort on the left side
+                MergeSort(Arr, Value, Middle, Right);//Merge sort on the right side
+                Merge(Arr, Left, Linq.Split(Arr, Left, Middle, Right), Value);//Merge to 2 sides together
             }
             else
             {
-                if (Value(Arr[Left]) > Value(Arr[Right]))
+                if (Value(Arr[Left]) > Value(Arr[Right]))//Swap the 2 values if they are the wrong way round
                 {
                     Swap(Arr, Left, Right);
                 }
@@ -24,13 +24,13 @@ namespace ImitationLibrary
 
         private static void Merge<T>(T[] Arr, int Left, T[][] SetsToMerge, Func<T, int> Value)
         {
-            int LeftLength = SetsToMerge[0].Length, RightLength = SetsToMerge[1].Length;
-            for (int i = Left, iL = 0, iR = 0; iL < LeftLength || iR < RightLength; i++)
+            int LeftLength = SetsToMerge[0].Length, RightLength = SetsToMerge[1].Length;//Determine the size of the regions we are merging
+            for (int i = Left, iL = 0, iR = 0; iL < LeftLength || iR < RightLength; i++)//Iterate until we have merged all values on both regions
             {
-                if (iL >= LeftLength) { Arr[i] = SetsToMerge[1][iR]; iR++; }
+                if (iL >= LeftLength) { Arr[i] = SetsToMerge[1][iR]; iR++; }//If one side is completely searched through, just keep itterating the other side
                 else if (iR >= RightLength) { Arr[i] = SetsToMerge[0][iL]; iL++; }
                 else if (Value(SetsToMerge[0][iL]) > Value(SetsToMerge[1][iR]))
-                {
+                {//Check if values are in the right place, and move them if they are not, then itterate the appropriate side
                     Arr[i] = SetsToMerge[1][iR];
                     iR++;
                 }
@@ -43,7 +43,7 @@ namespace ImitationLibrary
         }
 
         private static void Swap<T>(T[] Arr, int i1, int i2)
-        {
+        {//Swap 2 idexes around
             T Temp = Arr[i1];
             Arr[i1] = Arr[i2];
             Arr[i2] = Temp;
